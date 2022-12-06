fdcce5a9bf9933e876bf0a636c9d713d8eb750d4
Noah Lidell <noahlidell@gmail.com>
2022-12-05 23:31:43 -0600

    day05

--- app/Main.hs
+++ app/Main.hs
@@ -2,9 +2,17 @@ import           Data.Maybe                     ( fromJust )
 import qualified Day01
 import qualified Day02
 import qualified Day03
+import qualified Day04
+import qualified Day05
 import           System.Environment             ( getArgs )
 
-dispatch = [("Day01", Day01.main), ("Day02", Day02.main), ("Day03", Day03.main)]
+dispatch =
+    [ ("Day01", Day01.main)
+    , ("Day02", Day02.main)
+    , ("Day03", Day03.main)
+    , ("Day04", Day04.main)
+    , ("Day05", Day05.main)
+    ]
 
 main :: IO ()
 main = do
--- src/Day05.hs
+++ src/Day05.hs
@@ -1,4 +1,29 @@
+{-# LANGUAGE NamedFieldPuns #-}
 module Day05 where
+import           Data.Char                      ( isDigit )
+import           Data.List                      ( groupBy )
+import           Data.Map                       ( Map
+                                                , adjust
+                                                , elems
+                                                , empty
+                                                , fromList
+                                                , insert
+                                                , insertWith
+                                                , lookup
+                                                )
+import           Prelude                 hiding ( lookup )
+
+data Move = Move
+    { amt  :: Int
+    , from :: Int
+    , to   :: Int
+    }
+    deriving Show
+
+parseMove :: [String] -> Move
+parseMove [_, amt, _, from, _, to] = Move (int amt) (int from) (int to)
+    where int = read :: String -> Int
+parseMove _ = error "malformed"
 
 parseRow :: String -> [Char]
 parseRow ('['       : chr : ']' : row) = chr : parseRow row
@@ -8,6 +33,22 @@ parseRow (' '       : ' ' : ' ' : row) = ' ' : parseRow row
 parseRow []                            = []
 parseRow _                             = error "malformed"
 
+
+insertRow :: Map Int String -> [(Int, [Char])] -> Map Int String
+insertRow = foldl (\ma (k, v) -> insertWith (++) k v ma)
+
+craneLift :: Map Int String -> Move -> Map Int String
+craneLift m Move { amt = 0 }       = m
+craneLift m Move { amt, from, to } = craneLift updated $ Move (amt - 1) from to
+  where
+    updated                  = adjust tail from $ insertWith (++) to [crate] m
+    Just (crate : fromStack) = lookup from m
+
+craneLift9001 :: Map Int String -> Move -> Map Int String
+craneLift9001 m Move { amt, from, to } = insert from fromStack
+    $ insertWith (++) to crates m
+    where Just (crates, fromStack) = splitAt amt <$> lookup from m
+
 main :: IO ()
 main = do
     -- content <- readFile "data/day05test.txt"
@@ -15,13 +56,22 @@ main = do
     let moveLines =
             reverse . takeWhile (\ln -> take 1 ln == "m") . reverse $ lines
                 content
-            -- reverse . takeWhile (\(m : _) -> m == 'm') . reverse $ lines content
-    let
-        stackLines =
-            map parseRow
+    let moves =
+            map (parseMove . groupBy (\x y -> isDigit x == isDigit y)) moveLines
+    let stackLines =
+            map
+                    ( map (\(k, v) -> (k, [v]))
+                    . filter (\(k, v) -> v /= ' ')
+                    . zip [1 ..]
+                    . parseRow
+                    )
                 . reverse
                 . takeWhile (\ln -> take 2 ln /= " 1")
                 $ lines content
-    print moveLines
-    print stackLines
+    let stacks = foldl insertRow empty stackLines
+    let p1     = map head . elems $ foldl craneLift stacks moves
+    let p2     = map head . elems $ foldl craneLift9001 stacks moves
+    print p1
+    print p2
+
 
